6dc1246f57437231e36ab6f8eb5eccee865cb7cd
tonbaharry <tubotonba.harry@aol.com>
2022-06-27 15:17:42 -0400

    added api project

---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,67 @@
+﻿using AdvertApi.models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+        
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return new NotFoundResult() { };
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                throw;
+            }
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return new NotFoundResult() { };
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                throw;
+            }
+            return new OkResult(); //
+        }
+    }
+}
